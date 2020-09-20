e48fef22e23fb4a0c5bc06dceb2eb5324595a6a9
FtrSpec <56469806+FtrSpec@users.noreply.github.com>
2020-09-20 06:16:25 -0500

    Add files via upload
    Initial Beta

---
+++ background.js
@@ -0,0 +1,74 @@
+var swapList = [];
+var KEY = "DOMAINSWAPKEY"
+
+browser.webRequest.onBeforeRequest.addListener(swapDomain, {urls: ["<all_urls>"]}, ["blocking"]);
+getSwapList();
+
+function swapDomain(info) {
+    let lnkUrl = new webkitURL(info.url);
+    let item = swapIt(lnkUrl.host);
+    //console.log("swapDomain:swapIt(" + lnkUrl.host + ") = " + item);
+    if (item != null) {
+        browser.tabs.get(info.tabId, function (tab) {
+            if (info.type == "main_frame" & tab != undefined) {
+                const sourceDest = item.split(":");
+                browser.tabs.update(info.tabId, {url: info.url.replace(sourceDest[0], sourceDest[1])});
+                //console.log(sourceDest[0] +":" + sourceDest[1]);
+                return {cancel: true};
+            }
+        });
+    }
+}
+
+function swapIt(url) {
+    for (let i = 0; i < swapList.length; i++) {
+        const sourceDest = swapList[i].split(":");
+        if (url.includes(sourceDest[0])) {
+            return swapList[i];
+        }
+    }
+    return null;
+}
+
+function addSwap(item) {
+    swapList.push(item.toLowerCase());
+    swapList.sort();
+    let toSave = {};
+    toSave[KEY] = JSON.stringify(swapList);
+    browser.storage.local.set(toSave, function () {
+        console.log("Added")
+    });
+}
+
+function del(item, callback) {
+    swapList.splice(swapList.indexOf(item.toLowerCase()), 1);
+    swapList.sort();
+
+    let toSave = {};
+    toSave[KEY] = JSON.stringify(swapList);
+    browser.storage.local.set(toSave, callback);
+}
+
+function cleanlist() {
+    var tempList = []
+    for (var i = 0; i < swapList.length; i++) {
+        if (swapList[i].length > 2) {
+            tempList.push(swapList[i])
+        }
+    }
+    swapList = tempList;
+}
+
+function getSwapList() {
+    browser.storage.local.get(KEY,
+        function (item) {
+            if (Object.keys(item).length === 0) { //not found
+                console.log("Domain Swap List not found");
+                swapList = [];
+            } else {
+                swapList = JSON.parse(item[KEY]);
+                cleanlist();
+            }
+        }
+    );
+}
---
+++ popup.js
@@ -0,0 +1,46 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+    //console.debug("DOMContentLoaded");
+    cmdAdd.addEventListener("click", function() { addSwap(); });
+    cmdDel.addEventListener("click", function() { del(); });
+    fillList();
+});
+
+function addSwap(){
+	//console.debug("Adding");
+	const add = document.getElementById("txtSource").value + ":" + document.getElementById("txtDest").value;
+	//console.debug("Adding " + add);
+	if (add.length < 2){
+		console.log("Must be at least 2 characters.");
+		return;
+	}
+	browser.extension.getBackgroundPage().addSwap(add);
+	fillList();
+	document.getElementById("txtSource").value = "";
+	document.getElementById("txtDest").value = "";
+}
+
+function del(){
+	const item = swapList.value;
+	//console.debug("del " + item);
+	if (item != ""){
+		const sourceDest = item.split(":");
+		document.getElementById("txtSource").value = sourceDest[0];
+		document.getElementById("txtDest").value = sourceDest[1];
+		browser.extension.getBackgroundPage().del(item,fillList);
+		console.log("Deleted")
+	}
+}
+
+function fillList(){
+	swapList.innerHTML = "";
+	const swaps = browser.extension.getBackgroundPage().swapList;
+	if (swaps != []){
+		for (let i = 0; i < swaps.length; i++){
+			let opt = document.createElement("option");
+			opt.value = swaps[i];
+			opt.innerHTML = swaps[i];
+			swapList.appendChild(opt);
+		}
+	}
+}
+
